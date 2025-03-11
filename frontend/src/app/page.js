036c19c1337390a48e64605aad58d53639600dd6
SAHIL YADAV <121011155+sahil-kumar-yadav@users.noreply.github.com>
2025-03-11 12:04:24 +0000

    Authentication with NextAuth.js, Configure NextAuth.js API route,Wrap your App with Session Provider

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from "@/utils/queryClient";
 
 export default function Home({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <Component {...pageProps} /> */}
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </QueryClientProvider>
   );
 }
